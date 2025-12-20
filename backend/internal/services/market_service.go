46ac33116b6d25c919372e7653a03562d933e691
the-onewho-knocks <borsehardik@gmail.com>
2025-12-20 19:53:34 +0530

    added .gitignore i forgor at first

--- backend/internal/services/market_service.go
+++ backend/internal/services/market_service.go
@@ -10,9 +10,31 @@ type MarketService struct {
 	stockCache *cache.StockCache
 }
 
+// constructor
 func NewMarketService(api stockapi.Client, stockCache *cache.StockCache) *MarketService {
 	return &MarketService{
 		api:        api,
 		stockCache: stockCache,
 	}
+}
+
+// get a sigle stock price(uses redis and fallback api)
+func (s *MarketService) GetPrice(symbol string) (*stockapi.PriceData, error) {
+	price, err := s.stockCache.GetPrice(symbol)
+	if err == nil && price > 0 {
+		change, _ := s.stockCache.GetChange(symbol)
+		return &stockapi.PriceData{
+			Symbol: symbol,
+			Price:  price,
+			Change: change,
+		}, nil
+	}
+}
+
+func (s *MarketService) GetPrices(symbol []string) ([]stockapi.PriceData, error) {
+
+}
+
+func (s *MarketService) PriceStream(symbols []string) (<-chan PriceData, error) {
+
 }
